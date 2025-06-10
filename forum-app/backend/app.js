abdebade2d93ce8f3b181ade83956c84a63e35d7
fazrisuhada007 <fazrisuhada3@gmail.com>
2025-06-10 20:58:05 +0700

    feat(BE and FE): Mengatur ulang server side dengan vite

--- forum-app/backend/app.js
+++ forum-app/backend/app.js
@@ -15,7 +15,7 @@ const app = express();
 const port = 3000;
 
 // middleware
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 app.use(cookieParser());
@@ -25,6 +25,9 @@ if(process.env.NODE_ENV === 'development') {
 
 // routes
 app.use('/api/v1/auth', authRouter);
+app.get('/api/v1/test', (req, res) => {
+  res.status(200).json({success: true, message: 'Message from backend!'});
+})
 
 // error handler
 app.use(notFound);
--- forum-app/frontend/src/App.vue
+++ forum-app/frontend/src/App.vue
@@ -3,6 +3,16 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import Header from "./layouts/navbar/Header.vue";
+import customAxios from "./api.js"
+
+const getResponseBE = async () => {
+  const data = await customAxios.get("/test");
+  console.log(data);
+}
+
+onMounted(() => {
+  getResponseBE();
+})
 </script>
