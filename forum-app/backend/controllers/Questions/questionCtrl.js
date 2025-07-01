c131a58af34da1cd942a804bcef4b43b5502a4ec
fazrisuhada007 <fazrisuhada3@gmail.com>
2025-07-01 20:31:59 +0700

    feat(backend): get questions and detail question

--- forum-app/backend/controllers/Questions/questionCtrl.js
+++ forum-app/backend/controllers/Questions/questionCtrl.js
@@ -1,11 +1,42 @@
 import asyncHandler from "../../middlewares/asyncHandler.js";
 import questionMdl from "../../models/questionMdl.js";
+import mongoose from 'mongoose';
 
-export const allQuestions = (req, res) =>{
-    res.status(200).json('all questions');
-}
+export const allQuestions = asyncHandler(async(req, res) =>{
+    const getQuestions = await questionMdl.find();
+    
+    res.status(200).json({
+        success: true,
+        message: 'Get all questions successfully.',
+        data: getQuestions
+    });
+})
 
-export const detailQuestion = () =>{}
+export const detailQuestion = asyncHandler(async(req, res) =>{
+    const questionId = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(questionId)) {
+        return res.status(404).json({
+            success: false,
+            message: 'Question not found.'
+        });
+    }
+
+    const detailQuestion = await questionMdl.findById(questionId);
+
+    if(!detailQuestion) {
+        return res.status(404).json({
+            success: false,
+            message: 'Question not found.'
+        });
+    }
+
+    res.status(200).json({
+        success: true,
+        message: 'Get detail question successfully.',
+        data: detailQuestion
+    });
+})
 
 export const storeQuestion = asyncHandler(async(req, res) =>{
     const { title, content, category} = req.body;
